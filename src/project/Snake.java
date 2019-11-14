87b5d8278213a8fec3d5ec78210db65d87547ddc
Doug Shook <dsshook@gmail.com>
2019-11-14 16:13:14 -0600

    project starter code

---
+++ src/project/Snake.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Snake {
+	
+	//TODO: Implement the game of snake
+	public static void main(String[] args) {
+		
+		System.out.println("I'm a snaaaaaaaake! HISSSSSSSSSSSssssss.....");
+		
+	}
+	
+}
